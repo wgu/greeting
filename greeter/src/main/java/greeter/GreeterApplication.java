21d9d1442620c86855f55877a47486bdc1f77fe8
William Gu <williamgusien@gmail.com>
2017-06-01 16:23:02 -0700

    change to container to container service communication after service discovery

--- greeter/src/main/java/greeter/GreeterApplication.java
+++ greeter/src/main/java/greeter/GreeterApplication.java
@@ -1,52 +1,37 @@
 package greeter;
 
-import java.net.URI;
-
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 @RestController
 public class GreeterApplication {
 
-    @Bean
-    @LoadBalanced
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
-
     @Autowired
-    private RestTemplate rest;
+    MessageGenerationClient messageGeneration;
+
+    @RequestMapping(value = "/hello", method = GET)
+    public String greeting(@RequestParam(value="salutation", defaultValue="Hello") String salutation, @RequestParam(value="name", defaultValue="Bob") String name) {
+      Greeting greeting =  messageGeneration.greeting(name, salutation);
+      return greeting.getMessage();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(GreeterApplication.class, args);
-    }
-
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String hello(@RequestParam(value="salutation", defaultValue="Hello") String salutation, @RequestParam(value="name", defaultValue="Bob") String name) {
-        URI uri = UriComponentsBuilder.fromUriString("//message-generation/greeting")
-            .queryParam("salutation", salutation)
-            .queryParam("name", name)
-            .build()
-            .toUri();
-
-        Greeting greeting = rest.getForObject(uri, Greeting.class);
-        return greeting.getMessage();
     }
 
     private static class Greeting {
@@ -64,4 +49,9 @@ public class GreeterApplication {
 
     }
 
+    @FeignClient("https://message-generation")
+    interface MessageGenerationClient {
+      @RequestMapping(value = "/greeting", method = GET)
+      Greeting greeting(@RequestParam("name") String name, @RequestParam("salutation") String salutation);
+    }
 }
